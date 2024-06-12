43405c7b45d366bc4ea41244ffc7f6618ef92452
Acheche_Achraf <achraf.acheche@etud.univ-jfc.fr>
2024-06-12 09:23:19 +0200

    query

--- statsJad/views.py
+++ statsJad/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+from django.db import connection
 
-def index(request):
-    return render(request, 'index.html')
+def protocol_list(request):
+    protocol_names = [
+        "Entretien courant",
+        "Chambre à blanc",
+        "Entretien courant Expert+",
+        "Entretien courant Avancé"
+    ]
+    
+    protocol_names_str = ', '.join(f"'{name}'" for name in protocol_names)
+    
+    with connection.cursor() as cursor:
+        query = f"""
+            SELECT 
+                pe.id, pe.user_id, pe.start, pe."end", p.protocol_name
+            FROM 
+                protocol_event pe
+            INNER JOIN 
+                protocol p ON pe.protocol_id = p.protocol_id
+            WHERE 
+                p.protocol_name IN ({protocol_names_str})
+        """
+        cursor.execute(query)
+        protocol_events = cursor.fetchall()
+
+    return render(request, 'protocol.html', {'protocol_events': protocol_events})
