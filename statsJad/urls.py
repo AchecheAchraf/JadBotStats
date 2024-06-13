0ee39b9c677fd72df7174a8606aa45bba3a01069
Acheche_Achraf <achraf.acheche@etud.univ-jfc.fr>
2024-06-13 10:28:59 +0200

    extract all data

--- statsJad/urls.py
+++ statsJad/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import protocol_list_with_name, index,form
+from .views import protocol_list_with_name, index,form,protocol
 
 urlpatterns = [
-    path('protocol/', protocol_list_with_name, name='protocol_events'),
+    path('protocol/', protocol, name='protocol'),
     path('index/', index, name='index'),
     path('form/', form, name='form'),
     # Add other URL patterns as needed
--- statsJad/views.py
+++ statsJad/views.py
@@ -30,40 +30,6 @@ def protocol_list_with_name(request):
 
     return render(request, 'protocol.html', {'protocol_events': protocol_events})
 
-def protocol_list_with_name_last_month(request):
-    today = datetime.today()
-    first_day_of_this_month = today.replace(day=1)
-    last_day_of_last_month = first_day_of_this_month - timedelta(days=1)
-    first_day_of_last_month = last_day_of_last_month.replace(day=1)
-
-    specific_protocol_name = "Entretien courant"
-    
-    with connection.cursor() as cursor:
-        query = """
-            SELECT 
-                pe.id AS event_id, 
-                pe.user_id, 
-                pe.start, 
-                pe."end", 
-                p.protocol_name,
-                pe.ehpad_id
-            FROM 
-                protocol_event pe
-            INNER JOIN 
-                protocol p ON pe.protocol_id = p.protocol_id
-            WHERE 
-                p.protocol_name = %s
-                AND pe.start >= %s
-                AND pe."end" <= %s
-        """
-        cursor.execute(query, [specific_protocol_name, first_day_of_last_month, last_day_of_last_month])
-        columns = [col[0] for col in cursor.description]
-        protocol_events = [
-            dict(zip(columns, row))
-            for row in cursor.fetchall()
-        ]
-
-    return render(request, 'protocol.html', {'protocol_events': protocol_events})
 
 def protocol_list_last_month(request):
     today = datetime.today()
@@ -144,6 +110,80 @@ def protocol_list(request):
 
 def index(request):
     return render(request, 'index.html')
+
+from datetime import datetime
+
+def protocol(request):
+    # Get values from the form
+    start_date_str = request.POST.get('date-start')
+    end_date_str = request.POST.get('date-end')
+    protocol_name = request.POST.get('protocol-select')
+
+    # Convert form dates to datetime objects
+    start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+
+    # Adjust to the appropriate format for SQL
+    start_date_sql_format = start_date.strftime('%B %d, %Y, %I:%M %p')
+    end_date_sql_format = end_date.strftime('%B %d, %Y, %I:%M %p')
+
+    # Define the list of all protocol names
+    all_protocols = [
+        "Entretien courant",
+        "Chambre à blanc",
+        "Entretien courant Expert+",
+        "Entretien courant Avancé"
+    ]
+
+    with connection.cursor() as cursor:
+        if protocol_name == "Tous les protocols":
+            # If "Tous les protocols" is selected, search for all protocols
+            query = f"""
+                SELECT 
+                    pe.id AS event_id, 
+                    pe.user_id, 
+                    pe.start, 
+                    pe."end", 
+                    p.protocol_name,
+                    EXTRACT(EPOCH FROM (pe."end" - pe.start)) / 60 AS duration_minutes
+                FROM 
+                    protocol_event pe
+                INNER JOIN 
+                    protocol p ON pe.protocol_id = p.protocol_id
+                WHERE 
+                    p.protocol_name IN %s
+                    AND pe.start >= %s
+                    AND pe."end" <= %s
+            """
+            cursor.execute(query, [tuple(all_protocols), start_date_sql_format, end_date_sql_format])
+        else:
+            # Otherwise, search for the specific protocol selected
+            query = """
+                SELECT 
+                    pe.id AS event_id, 
+                    pe.user_id, 
+                    pe.start, 
+                    pe."end", 
+                    p.protocol_name,
+                    EXTRACT(EPOCH FROM (pe."end" - pe.start)) / 60 AS duration_minutes
+                FROM 
+                    protocol_event pe
+                INNER JOIN 
+                    protocol p ON pe.protocol_id = p.protocol_id
+                WHERE 
+                    p.protocol_name = %s
+                    AND pe.start >= %s
+                    AND pe."end" <= %s
+            """
+            cursor.execute(query, [protocol_name, start_date_sql_format, end_date_sql_format])
+
+        columns = [col[0] for col in cursor.description]
+        protocol_events = [
+            dict(zip(columns, row))
+            for row in cursor.fetchall()
+        ]
+
+    return render(request, 'protocol.html', {'protocol_events': protocol_events})
 
 
 def form(request):
