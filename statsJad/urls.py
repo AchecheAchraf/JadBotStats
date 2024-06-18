6028236acb7610e6f642474e595cedc778a86172
Acheche_Achraf <achraf.acheche@etud.univ-jfc.fr>
2024-06-18 11:46:06 +0200

    graph

--- statsJad/urls.py
+++ statsJad/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import protocol_list_with_name, index,form,protocol,protocolrooms
+from .views import protocol_list_with_name, index,form,protocol,protocolrooms,protocolday
 
 urlpatterns = [
-    path('protocolrooms/', protocolrooms, name='protocolrooms'),
+    # path('protocolrooms/', protocolrooms, name='protocolrooms'),
+    # path('protocolday/', protocolday, name='protocolday'),
     path('protocol/', protocol, name='protocol'),
     path('index/', index, name='index'),
     path('form/', form, name='form'),
